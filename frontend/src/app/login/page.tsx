a9196860aec39dad233c25613d36d82af50f9067
Valentin Garnier <valentingarnier@live.fr>
2026-02-09 09:37:56 +0100

    login issue

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -11,6 +11,9 @@ export default function LoginPage() {
       provider: "google",
       options: {
         redirectTo: `${window.location.origin}/auth/callback`,
+        queryParams: {
+          prompt: "select_account",
+        },
       },
     });
   };
